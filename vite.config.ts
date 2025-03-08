fea3b1227bd98b32e6be59b79975d63da5f80359
Andrew Dong <andrewdong1994@gmail.com>
2025-03-08 14:21:46 -0500

    Set up GitHub Pages deployment with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { plugin as markdownPlugin, Mode } from "vite-plugin-markdown";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     markdownPlugin({
